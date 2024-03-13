34080c0e2da93574d3bf90a7b039962c69a8db7b
VITALIYVY <verkash.vitalii@chnu.edu.ua>
2024-03-14 01:37:34 +0200

    all

--- Lab6CSharp/Program.cs
+++ Lab6CSharp/Program.cs
@@ -1,58 +1,202 @@
-﻿// See https://aka.ms/new-console-template for more information
-/// <summary>
-///  Top-level statements 
-///  Код програми (оператори)  вищого рівня
-/// </summary>
-///
-Console.WriteLine("Lab6 C# ");
-AnyFunc();
+﻿using System;
 
-/// <summary>
-/// 
-///  Top-level statements must precede namespace and type declarations.
-/// At the top-level methods/functions can be defined and used
-/// На верхньому рівні можна визначати та використовувати методи/функції
-/// </summary>
-void AnyFunc()
+// Завдання 2
+interface ITrans
 {
-    Console.WriteLine(" Some function in top-level");
+    void ShowInfo();
+    int GetLoadCapacity();
 }
-Console.WriteLine("Problems 1 ");
-AnyFunc();
-//  приклад класів
-UserClass cl = new UserClass();
-cl.Name = " UserClass top-level ";
-User.UserClass cl2 = new();
-cl2.Name = " UserClass namespace User ";
 
-
-
-
-/// <summary>
-/// 
-/// Top-level statements must precede namespace and type declarations.
-/// Оператори верхнього рівня мають передувати оголошенням простору імен і типу.
-/// Створення класу(ів) або оголошенням простору імен є закіченням  іструкцій верхнього рівня
-/// 
-/// </summary>
-
-namespace User
+class Car : ITrans
 {
-    class UserClass
+    private string brand;
+    private string number;
+    private int speed;
+    private int loadCapacity;
+
+    public Car(string brand, string number, int speed, int loadCapacity)
     {
-        public string Name { get; set; }
-        public UserClass()
-        {
-            Name = "NoName";
-        }
-        UserClass(string n)
-        {
-            Name = n;
-        }
+        this.brand = brand;
+        this.number = number;
+        this.speed = speed;
+        this.loadCapacity = loadCapacity;
     }
 
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Car: Brand - {brand}, Number - {number}, Speed - {speed}, Load Capacity - {loadCapacity}");
+    }
+
+    public int GetLoadCapacity()
+    {
+        return loadCapacity;
+    }
 }
-class UserClass
+
+class Motorcycle : ITrans
 {
-    public string Name { get; set; }
+    private string brand;
+    private string number;
+    private int speed;
+    private int loadCapacity;
+    private bool hasSidecar;
+
+    public Motorcycle(string brand, string number, int speed, int loadCapacity, bool hasSidecar)
+    {
+        this.brand = brand;
+        this.number = number;
+        this.speed = speed;
+        this.loadCapacity = hasSidecar ? loadCapacity : 0;
+        this.hasSidecar = hasSidecar;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Motorcycle: Brand - {brand}, Number - {number}, Speed - {speed}, Load Capacity - {loadCapacity}");
+    }
+
+    public int GetLoadCapacity()
+    {
+        return loadCapacity;
+    }
+}
+
+class Truck : ITrans
+{
+    private string brand;
+    private string number;
+    private int speed;
+    private int loadCapacity;
+    private bool hasTrailer;
+
+    public Truck(string brand, string number, int speed, int loadCapacity, bool hasTrailer)
+    {
+        this.brand = brand;
+        this.number = number;
+        this.speed = speed;
+        this.loadCapacity = hasTrailer ? loadCapacity * 2 : loadCapacity;
+        this.hasTrailer = hasTrailer;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Truck: Brand - {brand}, Number - {number}, Speed - {speed}, Load Capacity - {loadCapacity}");
+    }
+
+    public int GetLoadCapacity()
+    {
+        return loadCapacity;
+    }
+}
+
+// Завдання 1
+interface IUserInterface
+{
+    void Display();
+}
+
+interface IDotNetInterface
+{
+    void Method();
+}
+
+class Detail : IUserInterface, IDotNetInterface
+{
+    public void Display()
+    {
+        Console.WriteLine("Displaying Detail");
+    }
+
+    public void Method()
+    {
+        Console.WriteLine("Executing Method");
+    }
+}
+
+class Mechanism : IUserInterface, IDotNetInterface
+{
+    public void Display()
+    {
+        Console.WriteLine("Displaying Mechanism");
+    }
+
+    public void Method()
+    {
+        Console.WriteLine("Executing Method");
+    }
+}
+
+class Product : IUserInterface, IDotNetInterface
+{
+    public void Display()
+    {
+        Console.WriteLine("Displaying Product");
+    }
+
+    public void Method()
+    {
+        Console.WriteLine("Executing Method");
+    }
+}
+
+class Node : IUserInterface, IDotNetInterface
+{
+    public void Display()
+    {
+        Console.WriteLine("Displaying Node");
+    }
+
+    public void Method()
+    {
+        Console.WriteLine("Executing Method");
+    }
+}
+
+// Завдання 3 (додавання стандартних інтерфейсів .NET перерахування)
+enum Weekdays
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Завдання 2
+        ITrans[] vehicles = new ITrans[] {
+            new Car("Toyota", "ABC123", 120, 500),
+            new Motorcycle("Honda", "DEF456", 150, 200, true),
+            new Truck("Volvo", "GHI789", 80, 1000, false)
+        };
+
+        foreach (var vehicle in vehicles)
+        {
+            vehicle.ShowInfo();
+        }
+
+        // Завдання 1
+        IUserInterface[] interfaces = new IUserInterface[] {
+            new Detail(),
+            new Mechanism(),
+            new Product(),
+            new Node()
+        };
+
+        foreach (var ui in interfaces)
+        {
+            ui.Display();
+        }
+
+        // Завдання 3
+        foreach (Weekdays day in Enum.GetValues(typeof(Weekdays)))
+        {
+            Console.WriteLine(day);
+        }
+    }
 }
